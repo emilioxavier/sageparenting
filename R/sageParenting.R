4c370195a17222bad6144fa73ead57dcd06fc7b9
Emilio Xavier Esposito <emilio.esposito@gmail.com>
2020-10-12 14:04:50 -0400

    initial

---
+++ R/sageParenting.R
@@ -0,0 +1,164 @@
+#' @title Sage Parenting
+#' @name sageparenting
+#' @description Just your average 1980s/1990s parent.
+#'
+#' @docType package
+#' @aliases sageparenting sageparenting-package
+
+NULL
+
+
+
+#' @title Sage Parenting
+#' @description Just like your parents, only in your R session.
+#' @details Are you a child of the 1980s or 1990s? Where your parents the
+#'   masters of crushing your soul and spirit? Do you miss their support? Ever
+#'   feel too good? Like you are living your best life? Fire off the command
+#'   `sage.parenting()` to be put back in your place. Nothing like
+#'   a comment from mom or dad to adjust your attitude.
+#'
+#' @return `string` with all the feels
+#' @export
+#'
+#' @examples
+#' set.seed(13)
+#' sage.parenting()
+#' # [1] "~~~Mutters 'Why me?'~~~ Can you do anything right? :-<"
+#'
+#' @author Emilio Xavier Esposito \email{emilio.esposito@@gmail.com}
+#'
+sage.parenting <- function() {
+
+   parenting <- paste(sample(x=exclamation, size=1),
+                      sample(x=phrase, size=1),
+                      sample(x=frown, size=1),
+                      sep=" ")
+
+   return(parenting)
+}
+
+#' @title Exclamations
+#' @description Collection of phrases used by parents to start a comment.
+#'   Not exported.
+#'
+#' @author Emilio Xavier Esposito \email{emilio.esposito@@gmail.com}
+#'
+exclamation <- c(
+   "Ohh. Oh! No.    ",
+   "GOD DAMN IT!",
+   "JESUS CHRIST!",
+   "Really?    ",
+   "Ugh.   ",
+   "Why?   ",
+   "~~~In a whispered shout~~~",
+   "~~~Mutters 'Why me?'~~~",
+   "~~~Mutters 'Why Dear God?'~~~",
+   "~~~Mutters 'Dear God, why?'~~~",
+   "~~~Muffled sob~~~",
+   "~~~Shouting~~~",
+   "~~~Sigh~~~",
+   "~~~silence~~~",
+   "~~~Through gritted teeth~~~",
+   "~~~Whisper shouting 'GOD DAMN IT!'~~~"
+)
+
+#' @title Phrases
+#' @description Collection of phrases muttered by parents.
+#'   Not exported.
+#'
+#' @author Emilio Xavier Esposito \email{emilio.esposito@@gmail.com}
+#'
+phrase <- c(
+   "Because I said so.",
+   "Can you not?",
+   "Can you do anything right?",
+   "Can't you do anything right?",
+   "Don't!",
+   "Don't make me raise my voice.",
+   "Don't make say it again.",
+   "Have you no respect for yourself and others?",
+   "Just a minute. [10 minutes past]",
+   "Just go to your room.",
+   "Just look at yourself.",
+   "I can't right now. Go to your room.",
+   "I don't want to hear it.",
+   "I expect better from you.",
+   "I didn't raise a spoiled brat.",
+   "I didn't raise a selfish brat.",
+   "I don't expect this kind of behavior from you. Your brother/sister? Yes. But not you.",
+   "I don't expect this kind of behavior from you. Your sister/brother? Yes. But not you.",
+   "I raised you to be better than this.",
+   "I really don't know what to say.",
+   "I thought you knew better.",
+   "I should've done it myself.",
+   "I want you to sit there and think about what you've done.",
+   "I will NOT have you disrespecting people like this!",
+   "If you don't stop acting like this, we will go home right now.",
+   "I'll give you something to cry about.",
+   "I'm not angry. I'm disappointed.",
+   "Look at me when I'm talking to you.",
+   "Stop being lazy.",
+   "Stop being selfish.",
+   "Stop being ungrateful.",
+   "What did I do to deserve this?",
+   "What did you think was going to happen?",
+   "What have I done to deserve this?",
+   "What more do you want from me?",
+   "What were you thinking? Oh, that's right. You weren't.",
+   "You better stop this behavior or YOU are never coming back here.",
+   "You better wipe that look off your face.",
+   "You should know better.",
+   "You will not disrespect me in my house.",
+   "Your friends don't live in this house.",
+   "Why do you keep doing this to me?",
+   "[Walks away shaking their head.]",
+   ""
+)
+
+
+#' @title Text-based Frowns
+#' @description Collection of text-based frowns to show additional displeasure.
+#'   Based on emojis found in the [List of emoticons on Wikipedia]
+#'   (https://en.wikipedia.org/wiki/List_of_emoticons)
+#'   Not exported.
+#'
+#' @author Emilio Xavier Esposito \email{emilio.esposito@@gmail.com}
+#'
+frown <- c(
+   ")-:",
+   ":-(",
+   "):",
+   ":c",
+   ":-<",
+   ":<",
+   ":-[",
+   ":[",
+   ":-||",
+   ">:[",
+   ":{",
+   ":@",
+   ":(",
+   ";(",
+   "D-:",
+   "D:<",
+   "D:",
+   "D8",
+   "D;",
+   "D=",
+   ":-X",
+   ":/",
+   ":-/",
+   "8-L",
+   ":L",
+   ":-L",
+   ":|",
+   ":|",
+   ">:/",
+   ":-c"
+   )
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,52 @@
+
+<!-- README.md is generated from README.Rmd. Please edit README.Rmd. -->
+
+# sageparenting: Mom and dad in your console.
+
+> “I’ll give you something to cry about.” – Dad, circa 1985
+
+[![Project Status: Active - The project has reached a stable, usable
+state and is being actively
+developed.](http://www.repostatus.org/badges/latest/active.svg)](http://www.repostatus.org/#active)
+[![CRAN\_Release\_Badge](http://www.r-pkg.org/badges/version-ago/sageparenting)](https://CRAN.R-project.org/package=sageparenting)
+[![CRAN\_Download\_Badge](http://cranlogs.r-pkg.org/badges/sageparenting)](https://CRAN.R-project.org/package=sageparenting)
+
+## Installing `sageparenting`
+
+`sageparenting` is available on
+[GitHub](https://github.com/emilioxavier/sageparenting) and on
+[CRAN](https://cran.r-project.org/package=sageparenting). To install it:
+
+``` r
+# The easiest way to get sageparenting is:
+install.packages("sageparenting")
+
+# Or get the development version from GitHub:
+install.packages("devtools")
+devtools::install_github("emilioxavier/sageparenting")
+```
+
+## How to use `sageparenting`
+
+``` r
+library(sageparenting)
+sage.parenting()
+#> [1] "~~~Muffled sob~~~ You better wipe that look off your face. :-["
+```
+
+## Have a suggestion? Need help? Found a bug?
+
+  - Contact [Emilio](https://github.com/emilioxavier) at
+    <emilio.esposito@gmail.com>
+  - Submit an [issue via
+    GitHub](https://github.com/exeResearch/sageparenting/issues)
+
+## Code of conduct
+
+Please note that this project is released with a [Contributor Code of
+Conduct](CONDUCT.md). By participating in this project you agree to
+abide by its terms.
+
+## License
+
+MIT © [Emilio Xavier Esposito](https://github.com/emilioxavier)
