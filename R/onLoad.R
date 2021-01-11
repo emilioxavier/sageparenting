701ab6ac16672e9e8ce3c2ced4585d9da3173638
Emilio Xavier Esposito <emilio.esposito@gmail.com>
2021-01-11 14:57:03 -0500

    startup message

---
+++ R/onLoad.R
@@ -0,0 +1,18 @@
+# ******************************************************************************
+#   load and attach message
+# ******************************************************************************
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("\n********************************************************")
+  packageStartupMessage("Note: This package is a cathartics exercise.")
+  packageStartupMessage("  Remember, just because our parents said hurtful things")
+  packageStartupMessage("  things to us doesn’t mean we have to continue the cycle.")
+  packageStartupMessage("  ")
+  packageStartupMessage("  Verbal abuse is a serious problem. If you feel your parent/")
+  packageStartupMessage("  gardian is being abusive towards you beyond just having a")
+  packageStartupMessage("  bad day, please contact https://dayoneservices.org/verbal-abuse/.")
+  packageStartupMessage("  ")
+  packageStartupMessage("  Please submit recommendations for sageparenting here:")
+  packageStartupMessage("  https://forms.gle/Kek9PZpfDpx1XnLL6")
+  packageStartupMessage("********************************************************\n")
+}
